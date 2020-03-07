670eeeb992d3ba19a97228212ffacdf382ba2997
Alex Toth <tothalex95@gmail.com>
2020-03-07 08:59:09 +0100

    domain - book

---
+++ domain/src/main/java/com/example/books/domain/Book.kt
@@ -0,0 +1,14 @@
+package com.example.books.domain
+
+import java.time.LocalDate
+
+data class Book(
+    val title: String,
+    val subtitle: String?,
+    val authors: List<String>?,
+    val publisher: String,
+    val publishedDate: LocalDate,
+    val description: String,
+    val pageCount: Int,
+    val averageRating: Double
+)
